9e0348b1fee59a711d92b48f396ebbddc7e2fce4
maya-nachiappan <maya.n102304@gmail.com>
2024-09-15 07:40:52 -0700

    program to store larger value in variable using typical if statement

--- CIS25 - assignment 11/main.cpp
+++ CIS25 - assignment 11/main.cpp
@@ -1,14 +1,29 @@
 //
-//  main.cpp
 //  CIS25 - assignment 11
 //
 //  Created by maya nachiappan on 9/15/24.
 //
+// program that ask the user to enter two values, store the larger value in a separate variable, and then prints it
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    int value1, value2, largeValue;
+    cout << "enter a value: " << endl;
+    cin >> value1;
+    cout << "enter the second value: " << endl;
+    cin >> value2;
+    if(value1>value2)
+    {
+        largeValue=value1;
+    }
+    else
+    {
+        largeValue=value2;
+    }
+    cout << "The larger value is: "<< largeValue << endl;
     return 0;
 }
+
+
